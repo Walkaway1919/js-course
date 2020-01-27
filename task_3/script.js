285268c3ceb02c069c2bde1d2c45da273f958b4e
Julia <walkaway1919@gmail.com>
2020-01-28 00:30:13 +0300

    task_3_hard

--- task_3/script.js
+++ task_3/script.js
@@ -1,40 +1,33 @@
-let money = prompt ('Ваш месячный доход?'),
-    income = 'пособие по безработице',
-    addExpenses = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую'),
-    deposit = confirm ('Есть ли у вас депозит в банке?'),
-    mission = 100000,
-    period = 5,
-    expenses1 = prompt ('Введите обязательную статью расходов?'),
-    amount1 = prompt ('Во сколько это обойдется?'),
-    expenses2 = prompt ('Введите обязательную статью расходов?'),
-    amount2 = prompt ('Во сколько это обойдется?'),
-    budgetMonth = (money - amount1 - amount2);
-console.log(budgetMonth);
-console.log(Math.ceil(mission / budgetMonth));
-let budgetDay = (Math.floor(budgetMonth / 30));
-console.log(budgetDay);
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log('"Период равен ' + period + ' месяцев"');
-console.log('"Цель заработать ' + mission + ' рублей"');
-console.log(addExpenses.toLowerCase().split(', '));
-console.log(money);
-console.log(addExpenses);
-console.log(deposit);
+let lang = 'en';
+if (lang == 'en') {
+  alert('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+} else if (lang == 'ru') {
+  alert ('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+} //else {
+//   alert ('Another language');
+// }
 
 
-if (budgetDay >= 1200) {
-  alert('У вас высокий уровень дохода');
-} else if (budgetDay >= 600) {
-  alert('У вас средний уровень дохода');
-} else if (budgetDay >= 0) {
-  alert('К сожалению у вас уровень дохода ниже среднего');
-} else {
-  alert('Что-то пошло не так');
+switch (lang) {
+  case 'en':
+    alert('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+    break;
+  case 'ru':
+    alert('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+    break;
+  // default:
+  //   alert('Another language');
 }
 
 
+let days = {
+  ru: 'Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье',
+  en: 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday',
+};
 
+lang = 'ru'; // ru || en
+console.log( days[lang] );
+
+let namePerson = 'Артем';
+namePerson == 'Артем' ? console.log('директор') : namePerson == 'Максим' ? console.log('преподаватель') : console.log('студент');
 
