6281ba983fc2b25d32a02ecd0fb1c78b12907b84
Julia <walkaway1919@gmail.com>
2020-01-28 23:43:57 +0300

    task_4_hard

--- task_4/script.js
+++ task_4/script.js
@@ -1,55 +1,13 @@
-let money = prompt ('Ваш месячный доход?', 50000),
-    income = 'пособие по безработице',
-    addExpenses = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую'),
-    deposit = confirm ('Есть ли у вас депозит в банке?'),
-    mission = 120000,
-    period = 5;
-
-let showTypeOf = function(data){
-  console.log(data, typeof(data));
-};
-
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-let expenses1 = prompt ('Введите обязательную статью расходов?', 'бензин'),
-    amount1 = prompt ('Во сколько это обойдется?', 5000),
-    expenses2 = prompt ('Введите обязательную статью расходов?', 'телефон'),
-    amount2 = prompt ('Во сколько это обойдется?', 200);
-
-console.log('Возможные расходы ' + addExpenses.toLowerCase().split(', '));
-
-
-let getExpensesMonth = function(){
-  return parseInt(amount1) + parseInt(amount2);
-}
-
-console.log('Расходы за месяц ' + getExpensesMonth());
-
-let getAccumulatedMonth = function(){
-  return parseInt(money) - getExpensesMonth();
-}
-
-let accumulatedMonth = getAccumulatedMonth();
-
-let getTargetMonth = function(){
-  return mission / accumulatedMonth;
-}
-
-let budgetDay = (Math.floor(accumulatedMonth / 30));
-console.log('Бюджет на день ' + budgetDay);
-
-let getStatusIncome = function(){
-  if (budgetDay >= 1200) {
-    console.log('У вас высокий уровень дохода');
-  } else if (budgetDay >= 600) {
-    console.log('У вас средний уровень дохода');
-  } else if (budgetDay >= 0) {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
-  } else {
-    console.log('Что-то пошло не так');
+const phrases = string => {
+  if (typeof(string) != 'string'){
+    return 'is not a string';
   }
+  string=string.trim();
+  if(string.length > 30){
+    string = string.slice(0,30);
+    string += '...';
+  }
+  return string;
 };
-getStatusIncome();
-console.log('Цель будет достигнута через ' + Math.ceil(getTargetMonth()) + ' месяцев');
+
+// console.log(phrases('Eiusmod tempor fugiat ipsum elit velit velit deserunt eu consequat magna dolor in amet. Dolor officia reprehenderit et irure. Tempor labore amet non Lorem excepteur esse est ad minim id non dolore.'));
